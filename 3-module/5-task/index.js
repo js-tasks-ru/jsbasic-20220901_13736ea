e4e51a5c9d2571c539700c1816bf23a91d9bee42
Arthur Akhmetov <13736ea@gmail.com>
2022-09-21 22:34:27 +0500

    Начиная со 2-го модуля 3 такс до 3 модуля 5 таска включительно

--- 3-module/5-task/index.js
+++ 3-module/5-task/index.js
@@ -1,3 +1,9 @@
 function getMinMax(str) {
   // ваш код...
+  let num = str.split(' ').filter(item => Number(item));
+  let result = {
+    min: Math.min(...num),
+    max: Math.max(...num)
+  };
+  return result;
 }
